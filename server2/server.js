8b5dda8e1823e99386ae797c00be4c86af8c28d8
Niraj Shah <nirajshah950@gmail.com>
2025-11-13 23:40:13 +0530

    Running the asynchronous functions inside the run method
    disconnecting the consumer on error.

--- server2/server.js
+++ server2/server.js
@@ -4,24 +4,31 @@ import { Kafka } from "kafkajs";
 
 const kafka = new Kafka({
   clientId: "e-comm",
-  brokers: ["localhost:9092"]
+  brokers: ["localhost:9092"],
+  connectionTimeout: 600,
 });
 
 const consumer = kafka.consumer({ groupId: "inventory" });
 
-await consumer.connect();
-await consumer.subscribe({ topic: "order-placed" });
+const run = async () => {
+  await consumer.connect();
+  await consumer.subscribe({ topic: "order-placed" });
 
-await consumer.run({
-  eachMessage: async ({ topic, partition, message }) => {
-    console.log({
-      topic,
-      partition,
-      value: message.value.toString(),
-    });
-  },
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log("UPDATE THE INVENTORY - ");
+      console.log(JSON.parse(message.value.toString()));
+    },
+  });
+};
+
+run().catch(console.error);
+
+process.on("SIGINT", async () => {
+  console.log("Disconnecting consumer...");
+  await consumer.disconnect();
+  process.exit(0);
 });
-
 
 const app = express();
 const server = http.createServer(app);
